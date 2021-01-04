e00317f91009eb8d63d25d4663ba764770fef0c2
Ijaz Ur Rahim ( The Alien ) <ijazkhan095@gmail.com>
2021-01-05 03:41:34 +0500

    Added little Description
    Added some little description to the main code.
    more to be added

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ import time,threading
 
 """
 
-FUNCTIONS TO BE ADDED
+1. MORE EXPLANATION TO BE ADDED
+2. GRAPHICS TO BE ADDED
+3. TO BE CONVERTED TO OOP
 
 """
 
 
 def thread(y,store,array={},statement = None):
+	"""
+		Thread that finds all non duplicate clients of a specified store in a specified year, executing the following example query
+		"SELECT DISTINCT(client_id) FROM heads WHERE YEAR(ticket_date) = 2016 AND store_id = 'ALB'"
+	"""
 	if  statement is None:
 		statement = select([	 func.distinct(heads.c.client_id)	]).\
 					where(	
@@ -24,11 +30,15 @@ def thread(y,store,array={},statement = None):
 					)
 	array[store] = []
 	with engine.connect() as conn:
-		[array[store].append(cid) for cid in conn.execute(statement).fetchall()]
+		[array[store].append(cid[0]) for cid in conn.execute(statement).fetchall()]
 	return array
 
 
 def getPerYear(conn,year,f):
+	"""
+		Gets max/min/(total cost)/(number of customer) per store in selected year, Executing the following example query out of four
+		"SELECT COUNT(DISTINCT(`client_id`)), `store_id` FROM `heads` WHERE YEAR(`ticket_date`) = 2017 GROUP BY `store_id`"
+	"""
 	statement = select([	f , heads.c.store_id	]).\
 				where(	func.year(heads.c.ticket_date) == year	).\
 				group_by(heads.c.store_id)
@@ -36,6 +46,10 @@ def getPerYear(conn,year,f):
 	return conn.execute(statement).fetchall()
 
 def getFromTo(conn,year1,year2,f):
+	"""
+		Gets max/min/(total cost)/(number of customer) per store between two dates, Executing the following example query out of four
+		"SELECT COUNT(DISTINCT(`client_id`)), `store_id` FROM `heads` WHERE `ticket_date` BETWEEN 2016 AND 2017 GROUP BY `store_id`"
+	"""
 	statement = select([	f,	heads.c.store_id	]).\
 				where(	heads.c.ticket_date.between(year1,year2)	).\
 				group_by(	heads.c.store_id	)
@@ -43,6 +57,10 @@ def getFromTo(conn,year1,year2,f):
 	return conn.execute(statement).fetchall()
 
 def getPerStore(conn,f):
+	"""	
+		Gets max/min/(total cost)/(number of customer) per store by month, Executing the following example query out of four
+		"SELECT COUNT(DISTINCT(`client_id`)), DATE_FORMAT(`ticket_date`, '%Y-%m') as date_log, QUARTER(`ticket_date`) FROM `heads` GROUP BY `store_id`, date_log"
+	"""
 	statement = select([	f,	heads.c.store_id,	func.date_format(heads.c.ticket_date,'%Y-%m').label('date_log'),	func.quarter(heads.c.ticket_date)	]).\
 				group_by(	heads.c.store_id	).\
 				group_by(	'date_log'	)
@@ -51,12 +69,24 @@ def getPerStore(conn,f):
 
 
 def getStores(conn):
+	"""
+		Gets all stores by executing the following query
+		"SELECT `store_id` FROM `stores`"
+	"""
+
 	statement = select([stores.c.store_id])
 	return conn.execute(statement).fetchall()
 
 
 
 def definingThread(conn,year,discount=False):
+	"""
+		Method that deals with creating threads and joining their data into one dictionary.
+		If there is a discount value to find (VIP CUSTOMERS)
+		the following example query is executed instead of the original.
+		"SELECT client_id,SUM(discount_volume) from heads WHERE YEAR(ticket_date) = 2016 AND store_id = 'ALB' AND discount_volume > 0 GROUP BY client_id"
+	"""
+
 	array ={}
 	threads = []
 	print("Getting Stores\n")
@@ -92,6 +122,11 @@ def definingThread(conn,year,discount=False):
 
 
 def getLoyals(conn,getClients=False):
+	"""
+		Finds the clients/customers in 2016 and 2017. then check if customers/clients present in 2016 is present in 2017 or not. 
+		If the client is available in both years then it is saved as loyal client
+	"""
+
 	lastYearClients = definingThread(conn,2016)
 	newYearClients = definingThread(conn,2017)
 	loyals = {}
@@ -113,6 +148,11 @@ def getLoyals(conn,getClients=False):
 
 
 def getAttritions(conn,getClients=False):
+	"""
+		Finds the clients/customers in 2016 and 2017. then check if customers/clients present in 2016 is present in 2017 or not. 
+		If not present then saves it as attrition/lost client to the store
+	"""
+
 	lastYearClients = definingThread(conn,2016)
 	newYearClients = definingThread(conn,2017)
 	attritions = {}
@@ -134,6 +174,11 @@ def getAttritions(conn,getClients=False):
 
 
 def getNew(conn,getClients=False):
+	"""
+		Finds the clients/customers in 2016 and 2017. then check if customers/clients present in 2017 is present in 2016 or not. 
+		If not present then saves it as new client to the store
+	"""
+
 	lastYearClients = definingThread(conn,2016)
 	newYearClients = definingThread(conn,2017)
 
@@ -155,10 +200,20 @@ def getNew(conn,getClients=False):
 
 
 def getMQY(conn,f):
+
+	"""
+	This function is divided into 3 parts.
+	1. Finds (number of clients)/max/min/(total sales) for each store in a month. Saves the quater dates and years for remainig parts
+	2. Finds (number of clients)/max/min/(total sales) for each store in a Quarter.
+	3. Finds (number of clients)/max/min/(total sales) for each store in a year.
+
+	"""
+
 	yearlyData = {}
 	quaterlyData = {}
 	monthlyData = {}
 
+	# part 1
 	years = []
 	quarters = {}
 	for count,store,date,quart in getPerStore(conn,f):
@@ -187,11 +242,13 @@ def getMQY(conn,f):
 				quaterlyData[d1] = {store:0}
 			else:
 				quaterlyData[d1][store] = 0
-			
+
+	# part 2
 	for q1,q2 in quarters.items():
 		for count,store in getFromTo(conn,q1,q2,f):
 			quaterlyData[q1][store] = count
 	
+	# part 3
 	for year in years:
 		for count,store in getPerYear(conn,year,f):
 			yearlyData[year][store] = count
@@ -199,18 +256,30 @@ def getMQY(conn,f):
 	return monthlyData,quaterlyData,yearlyData
 
 def getClientsCount(conn):
+	"""
+		finds number of clients/customers for each store in month, quater and year
+	"""
 	f = func.count( func.distinct(heads.c.client_id) ) 
 	return getMQY(conn,f)
 
 def getMax(conn):
+	"""
+		finds Maximum sales done by each store in month, quater and year
+	"""
 	f = func.max( heads.c.total_ticket_cost )
 	return getMQY(conn,f)
 	
 def getMin(conn):
+	"""
+		finds Minimum sales done by each store in month, quater and year
+	"""
 	f = func.min( heads.c.total_ticket_cost )
 	return getMQY(conn,f)
 	
 def getSales(conn):
+	"""
+		finds total sales done by each store in month, quater and year
+	"""
 	f = func.sum( heads.c.total_ticket_cost )
 	return getMQY(conn,f)
 	
@@ -219,7 +288,10 @@ def getSales(conn):
 
 
 def getVipCustomers(conn):
-	
+	"""
+		collects 2016 and 2017 clients and check if each clients done purchases in 2016 and 2017 and also granted the discounts
+	"""
+
 	lastYearClients = definingThread(conn,2016,discount=True)
 	newYearClients = definingThread(conn,2017,discount=True)
 
@@ -239,9 +311,11 @@ def getVipCustomers(conn):
 	return vip
 
 
-
-
 def getVipDiscounts(conn):
+	"""
+	Executes the following MySQL Query to get volume of discount coupons and grouping them by store_id
+	"SELECT SUM(`discount_volume`), `store_id` from `heads` WHERE YEAR(`ticket_date`) BETWEEN 2016 and 2017 GROUP BY `store_id`"
+	"""
 	statement = select([	func.sum(	heads.c.discount_volume ), heads.c.store_id	]).\
 				where(
 					and_(
@@ -255,6 +329,12 @@ def getVipDiscounts(conn):
 
 
 def getGrowthRate(conn):
+
+	"""
+		Gets clients done purchases only in 2017 (new clients) and divide them by total customers in previous year (2016)
+		to find	Growth Rate of the stores
+	"""
+
 	stores = getNew(conn)
 	f = func.count( func.distinct(heads.c.client_id) ) 
 	totalLastYear = {s:c for c,s in getPerYear(conn,2016,f)}
@@ -271,6 +351,11 @@ def getGrowthRate(conn):
 
 
 def getAttritionRate(conn):
+	"""
+		Gets clients done purchases only in 2016 (old clients) and divide them by average clients of the store (average = total clients/2)
+		to find	Attrition Rate of the stores
+	"""
+
 	stores = getAttritions(conn)
 	f = func.count( func.distinct(heads.c.client_id) ) 
 	averageClients = {s:c/2 for c,s in getFromTo(conn,2016,2017,f)}
@@ -284,6 +369,12 @@ def getAttritionRate(conn):
 
 
 def getRatio(conn):
+
+	"""
+		Finds the ration between clients and revenues for each store by executing the following query
+		"SELECT SUM(total_ticket_cost), COUNT(DISTINCT(client_id)), store_id from heads GROUP BY store_id"
+	"""
+
 	statement = select([	func.sum( heads.c.total_ticket_cost ), func.count(func.distinct(heads.c.client_id)) , heads.c.store_id	]).\
 				group_by(	heads.c.store_id	)
 
@@ -300,7 +391,7 @@ with engine.connect() as conn:
 	# getMax(conn)
 	# getMin(conn)
 	# getSales(conn)	
-	print(getGrowthRate(conn))	
+	# print(getGrowthRate(conn))	
 	# print(getAttritionRate(conn))	
 	# print(getRatio(conn))	
 	pass
